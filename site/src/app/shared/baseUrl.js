665d4d20419fbfac2c43ce2286383942b1aeebd7
Martyb15 <60498446+Martyb15@users.noreply.github.com>
2025-06-10 15:52:09 -0700

    Update baseUrl.js

--- site/src/app/shared/baseUrl.js
+++ site/src/app/shared/baseUrl.js
@@ -1,1 +1,2 @@
-export const baseUrl = 'http://localhost:3001/';
+// export const baseUrl = 'http://localhost:3001/';
+export const baseUrl = 'https://martinbarrios-json-api-9ca611dad1e6.herokuapp.com/';
